ee3da67956faa5e5d66f4a9430c2ae723073a85c
MD Shahriar Rahi <afmssrahi@gmail.com>
2020-12-09 19:01:24 +0600

    I written some code in app.js file to make dynamic progress-bar for Digital-Clock

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 const timePeriod = document.getElementById('timePeriod');
+const progress = document.getElementById('progress');
 
 function showCurrentTime() {
     let date = new Date();
@@ -13,6 +14,8 @@ function showCurrentTime() {
     minute.textContent = (min < 10) ? ('0' + min) : min;
     second.textContent = (sec < 10) ? ('0' + sec) : sec;
     timePeriod.textContent = (hr >= 12) ? 'pm' : 'am';
+
+    progress.style.width = (sec / 60) * 100 + '%';
 }
 
 setInterval(showCurrentTime, 1000);
